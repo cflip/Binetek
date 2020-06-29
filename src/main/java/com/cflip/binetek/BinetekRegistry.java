45c273479d2841bc252979c259ef375d9f21493c
jprik <60302563+jprik@users.noreply.github.com>
2020-06-29 13:05:22 -0600

    Added limestone block

--- src/main/java/com/cflip/binetek/BinetekRegistry.java
+++ src/main/java/com/cflip/binetek/BinetekRegistry.java
@@ -22,14 +22,16 @@ public class BinetekRegistry {
                 ItemList.fertilizerDust = new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName(Binetek.MODID, "fertilizer_dust"),
                 ItemList.fertilizerPouch = new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName(Binetek.MODID, "fertilizer_pouch"),
                 ItemList.coalCoke = new Item(new Item.Properties().group(ItemGroup.MATERIALS)).setRegistryName(Binetek.MODID, "coal_coke"),
-                ItemList.researchTable = new BlockItem(BlockList.researchTable, new Item.Properties().group(ItemGroup.DECORATIONS)).setRegistryName(BlockList.researchTable.getRegistryName())
+                ItemList.researchTable = new BlockItem(BlockList.researchTable, new Item.Properties().group(ItemGroup.DECORATIONS)).setRegistryName(BlockList.researchTable.getRegistryName()),
+                ItemList.limeStone = new BlockItem(BlockList.limeStone, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(BlockList.limeStone.getRegistryName())
         );
     }
 
     @SubscribeEvent
     public static void registerBlocks(final RegistryEvent.Register<Block> event) {
         event.getRegistry().registerAll(
-                BlockList.researchTable = new Block(Block.Properties.create(Material.WOOD).hardnessAndResistance(2.6f).harvestTool(ToolType.AXE).sound(SoundType.WOOD)).setRegistryName(Binetek.MODID, "research_table")
+                BlockList.researchTable = new Block(Block.Properties.create(Material.WOOD).hardnessAndResistance(2.6f).harvestTool(ToolType.AXE).sound(SoundType.WOOD)).setRegistryName(Binetek.MODID, "research_table"),
+                BlockList.limeStone = new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.6f).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName(Binetek.MODID, "limestone")
         );
     }
 }
