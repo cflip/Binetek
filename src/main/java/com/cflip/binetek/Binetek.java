b40fbd0b35c86ca99610050529d55b17db7d239c
jprik <60302563+jprik@users.noreply.github.com>
2020-06-30 10:55:15 -0600

    Added research books

--- src/main/java/com/cflip/binetek/Binetek.java
+++ src/main/java/com/cflip/binetek/Binetek.java
@@ -30,46 +30,50 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod(Binetek.MODID)
 public class Binetek {
-	public static final String MODID = "binetek";
+    public static final String MODID = "binetek";
 
-	public Binetek() {
-		MinecraftForge.EVENT_BUS.register(this);
-	}
+    public Binetek() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
 
-	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-	public static class RegistryEvents {
-		@SubscribeEvent
-		public static void loadComplete(FMLLoadCompleteEvent event) {
-			for (Biome biome : ForgeRegistries.BIOMES) {
-				ConfiguredPlacement placementConfig = Placement.COUNT_RANGE.configure(new CountRangeConfig(8, 0, 0, 16));
-				OreFeatureConfig featureConfig = new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockList.sulfurOre.getDefaultState(), 7);
-				biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Feature.ORE.withConfiguration(featureConfig).withPlacement(placementConfig));
-			}
-		}
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void loadComplete(FMLLoadCompleteEvent event) {
+            for (Biome biome : ForgeRegistries.BIOMES) {
+                ConfiguredPlacement placementConfig = Placement.COUNT_RANGE.configure(new CountRangeConfig(8, 0, 0, 16));
+                OreFeatureConfig featureConfig = new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockList.sulfurOre.getDefaultState(), 7);
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Feature.ORE.withConfiguration(featureConfig).withPlacement(placementConfig));
+            }
+        }
 
-		@SubscribeEvent
-		public static void registerItems(final RegistryEvent.Register<Item> event) {
-			event.getRegistry().registerAll(
-				// ITEM ITEMS
-				ItemList.fertilizerDust = new BoneMealItem(new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(Binetek.MODID, "fertilizer_dust"),
-				ItemList.fertilizerPouch = new FertilizerPouchItem(new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(Binetek.MODID, "fertilizer_pouch"),
-				ItemList.coalCoke = new CoalCokeItem(new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(Binetek.MODID, "coal_coke"),
-				ItemList.sulfur = new Item(new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(Binetek.MODID, "sulfur"),
+        @SubscribeEvent
+        public static void registerItems(final RegistryEvent.Register<Item> event) {
+            event.getRegistry().registerAll(
+                    // ITEM ITEMS
+                    ItemList.fertilizerDust = new BoneMealItem(new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(Binetek.MODID, "fertilizer_dust"),
+                    ItemList.fertilizerPouch = new FertilizerPouchItem(new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(Binetek.MODID, "fertilizer_pouch"),
+                    ItemList.coalCoke = new CoalCokeItem(new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(Binetek.MODID, "coal_coke"),
+                    ItemList.sulfur = new Item(new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(Binetek.MODID, "sulfur"),
+                    ItemList.techbookChem = new Item(new Item.Properties().group(BinetekItemGroup.instance).maxStackSize(1)).setRegistryName(Binetek.MODID, "techbook_chemical"),
+                    ItemList.techbookConst = new Item(new Item.Properties().group(BinetekItemGroup.instance).maxStackSize(1)).setRegistryName(Binetek.MODID, "techbook_construction"),
+                    ItemList.techbookOrd = new Item(new Item.Properties().group(BinetekItemGroup.instance).maxStackSize(1)).setRegistryName(Binetek.MODID, "techbook_order"),
+                    ItemList.techbookWeap = new Item(new Item.Properties().group(BinetekItemGroup.instance).maxStackSize(1)).setRegistryName(Binetek.MODID, "techbook_weaponry"),
 
-				// BLOCK ITEMS
-				ItemList.researchTable = new BlockItem(BlockList.researchTable, new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(BlockList.researchTable.getRegistryName()),
-				ItemList.limeStone = new BlockItem(BlockList.limeStone, new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(BlockList.limeStone.getRegistryName()),
-				ItemList.sulfurOre = new BlockItem(BlockList.sulfurOre, new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(BlockList.sulfurOre.getRegistryName())
-			);
-		}
+                    // BLOCK ITEMS
+                    ItemList.researchTable = new BlockItem(BlockList.researchTable, new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(BlockList.researchTable.getRegistryName()),
+                    ItemList.limeStone = new BlockItem(BlockList.limeStone, new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(BlockList.limeStone.getRegistryName()),
+                    ItemList.sulfurOre = new BlockItem(BlockList.sulfurOre, new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(BlockList.sulfurOre.getRegistryName())
+            );
+        }
 
-		@SubscribeEvent
-		public static void registerBlocks(final RegistryEvent.Register<Block> event) {
-			event.getRegistry().registerAll(
-				BlockList.researchTable = new ResearchTableBlock().setRegistryName(Binetek.MODID, "research_table"),
-				BlockList.limeStone = new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.6f).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName(Binetek.MODID, "limestone"),
-				BlockList.sulfurOre = new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3).harvestTool(ToolType.PICKAXE).harvestLevel(2).sound(SoundType.STONE)).setRegistryName(Binetek.MODID, "sulfur_ore")
-			);
-		}
-	}
+        @SubscribeEvent
+        public static void registerBlocks(final RegistryEvent.Register<Block> event) {
+            event.getRegistry().registerAll(
+                    BlockList.researchTable = new ResearchTableBlock().setRegistryName(Binetek.MODID, "research_table"),
+                    BlockList.limeStone = new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.6f).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName(Binetek.MODID, "limestone"),
+                    BlockList.sulfurOre = new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3).harvestTool(ToolType.PICKAXE).harvestLevel(2).sound(SoundType.STONE)).setRegistryName(Binetek.MODID, "sulfur_ore")
+            );
+        }
+    }
 }
--- src/main/java/com/cflip/binetek/item/ItemList.java
+++ src/main/java/com/cflip/binetek/item/ItemList.java
@@ -3,7 +3,7 @@ package com.cflip.binetek.item;
 import net.minecraft.item.Item;
 
 public class ItemList {
-	public static Item fertilizerDust, coalCoke, fertilizerPouch, sulfur;
+	public static Item fertilizerDust, coalCoke, fertilizerPouch, sulfur, techbookChem, techbookConst, techbookOrd, techbookWeap;
 
 	public static Item researchTable, limeStone, sulfurOre;
 }
