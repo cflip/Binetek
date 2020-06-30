78f492490f2d41b10433ebd0cfa393e819f40aae
Cflip <36554078+cflip@users.noreply.github.com>
2020-06-30 13:16:15 -0600

    Add copper ore and ingots

--- src/main/java/com/cflip/binetek/Binetek.java
+++ src/main/java/com/cflip/binetek/Binetek.java
@@ -5,6 +5,7 @@ import com.cflip.binetek.block.ResearchTableBlock;
 import com.cflip.binetek.client.gui.ResearchTableScreen;
 import com.cflip.binetek.container.ContainerList;
 import com.cflip.binetek.container.ResearchTableContainer;
+import com.cflip.binetek.gen.BinetekOreGen;
 import com.cflip.binetek.item.BinetekItemGroup;
 import com.cflip.binetek.item.CoalCokeItem;
 import com.cflip.binetek.item.FertilizerPouchItem;
@@ -17,20 +18,12 @@ import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.BoneMealItem;
 import net.minecraft.item.Item;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.gen.GenerationStage;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.OreFeatureConfig;
-import net.minecraft.world.gen.placement.ConfiguredPlacement;
-import net.minecraft.world.gen.placement.CountRangeConfig;
-import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.common.ToolType;
 import net.minecraftforge.common.extensions.IForgeContainerType;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
-import net.minecraftforge.registries.ForgeRegistries;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
@@ -52,11 +45,7 @@ public class Binetek {
 
 		@SubscribeEvent
 		public static void loadComplete(FMLLoadCompleteEvent event) {
-			for (Biome biome : ForgeRegistries.BIOMES) {
-				ConfiguredPlacement placementConfig = Placement.COUNT_RANGE.configure(new CountRangeConfig(8, 0, 0, 16));
-				OreFeatureConfig featureConfig = new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockList.sulfurOre.getDefaultState(), 7);
-				biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Feature.ORE.withConfiguration(featureConfig).withPlacement(placementConfig));
-			}
+			BinetekOreGen.generate();
 		}
 
 		@SubscribeEvent
@@ -74,6 +63,7 @@ public class Binetek {
 				ItemList.fertilizerPouch = new FertilizerPouchItem(new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(Binetek.MODID, "fertilizer_pouch"),
 				ItemList.coalCoke = new CoalCokeItem(new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(Binetek.MODID, "coal_coke"),
 				ItemList.sulfur = new Item(new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(Binetek.MODID, "sulfur"),
+				ItemList.copperIngot = new Item(new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(Binetek.MODID, "copper_ingot"),
 				ItemList.techbookChem = new Item(new Item.Properties().group(BinetekItemGroup.instance).maxStackSize(1)).setRegistryName(Binetek.MODID, "techbook_chemical"),
 				ItemList.techbookConst = new Item(new Item.Properties().group(BinetekItemGroup.instance).maxStackSize(1)).setRegistryName(Binetek.MODID, "techbook_construction"),
 				ItemList.techbookOrd = new Item(new Item.Properties().group(BinetekItemGroup.instance).maxStackSize(1)).setRegistryName(Binetek.MODID, "techbook_order"),
@@ -81,8 +71,9 @@ public class Binetek {
 
 				// BLOCK ITEMS
 				ItemList.researchTable = new BlockItem(BlockList.researchTable, new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(BlockList.researchTable.getRegistryName()),
-				ItemList.limeStone = new BlockItem(BlockList.limeStone, new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(BlockList.limeStone.getRegistryName()),
-				ItemList.sulfurOre = new BlockItem(BlockList.sulfurOre, new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(BlockList.sulfurOre.getRegistryName())
+				ItemList.limeStone = new BlockItem(BlockList.limestone, new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(BlockList.limestone.getRegistryName()),
+				ItemList.sulfurOre = new BlockItem(BlockList.sulfurOre, new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(BlockList.sulfurOre.getRegistryName()),
+				ItemList.copperOre = new BlockItem(BlockList.copperOre, new Item.Properties().group(BinetekItemGroup.instance)).setRegistryName(BlockList.copperOre.getRegistryName())
 			);
 		}
 
@@ -90,8 +81,9 @@ public class Binetek {
 		public static void registerBlocks(final RegistryEvent.Register<Block> event) {
 			event.getRegistry().registerAll(
 				BlockList.researchTable = new ResearchTableBlock().setRegistryName(Binetek.MODID, "research_table"),
-				BlockList.limeStone = new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.6f).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName(Binetek.MODID, "limestone"),
-				BlockList.sulfurOre = new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3).harvestTool(ToolType.PICKAXE).harvestLevel(2).sound(SoundType.STONE)).setRegistryName(Binetek.MODID, "sulfur_ore")
+				BlockList.limestone = new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5f, 5).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName(Binetek.MODID, "limestone"),
+				BlockList.sulfurOre = new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3).harvestTool(ToolType.PICKAXE).harvestLevel(2).sound(SoundType.STONE)).setRegistryName(Binetek.MODID, "sulfur_ore"),
+				BlockList.copperOre = new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.8f).harvestTool(ToolType.PICKAXE).harvestLevel(1).sound(SoundType.STONE)).setRegistryName(Binetek.MODID, "copper_ore")
 			);
 		}
 	}
---
+++ src/main/java/com/cflip/binetek/gen/BinetekOreGen.java
@@ -0,0 +1,27 @@
+package com.cflip.binetek.gen;
+
+import com.cflip.binetek.block.BlockList;
+import net.minecraft.block.Block;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.placement.ConfiguredPlacement;
+import net.minecraft.world.gen.placement.CountRangeConfig;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class BinetekOreGen {
+	public static void generate() {
+		for (Biome biome : ForgeRegistries.BIOMES) {
+			addOre(biome, BlockList.sulfurOre, 8, 7, 0, 16);
+			addOre(biome, BlockList.copperOre, 21, 12, 0, 64);
+		}
+	}
+
+	public static void addOre(Biome biome, Block oreBlock, int rarity, int size, int minLevel, int maxLevel) {
+		ConfiguredPlacement placementConfig = Placement.COUNT_RANGE.configure(new CountRangeConfig(rarity, minLevel, 0, maxLevel));
+		OreFeatureConfig featureConfig = new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, oreBlock.getDefaultState(), size);
+		biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Feature.ORE.withConfiguration(featureConfig).withPlacement(placementConfig));
+	}
+}
